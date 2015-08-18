b5f06c3788fd8f074ed14d76d6ee94b590cdbb67
brozermanik <brozermanik@gmail.com>
2015-08-18 17:17:27 +0300

    1st push

---
+++ ExerciseTriangle/GetTriangleSquare.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseTriangle
+{
+    public class GetTriangleSquare
+    {
+        public static void TriangleSquare()
+        {
+            try
+            {
+                Console.WriteLine("Введите длину первой стороны треугольника в см");
+                double aSide = Convert.ToDouble(Console.ReadLine());
+                if ((aSide <= 0) || (aSide.GetTypeCode() != TypeCode.Double))
+                {
+                    throw new Exception("Введено неверное значение, длина стороны треугольника не может равняться нулю или быть отрицательным");
+                }
+
+                Console.WriteLine("Введите длину второй стороны треугольника в см");
+                double bSide = Convert.ToDouble(Console.ReadLine());
+                if ((bSide <= 0) && (bSide.GetTypeCode() != TypeCode.Double))
+                {
+                    throw new Exception("Введено неверное значение, длина стороны треугольника не может равняться нулю или быть отрицательным");
+                }
+                double trSquare = (aSide * bSide) / 2;
+                Console.WriteLine("Таким образом площадь треугольника равно {0:0.0} см в квадрате", trSquare);
+                Console.ReadKey();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        public static double TriangleSquare(double aSide, double bSide)
+        {
+            try
+            {
+                Console.WriteLine("Введите длину первой стороны треугольника в см");
+                aSide = Convert.ToDouble(Console.ReadLine());
+                if ((aSide <= 0) || (aSide.GetTypeCode() != TypeCode.Double))
+                {
+                    throw new Exception("Введено неверное значение, длина стороны треугольника не может равняться нулю или быть отрицательным");
+                }
+
+                Console.WriteLine("Введите длину второй стороны треугольника в см");
+                bSide = Convert.ToDouble(Console.ReadLine());
+                if ((bSide <= 0) && (bSide.GetTypeCode() != TypeCode.Double))
+                {
+                    throw new Exception("Введено неверное значение, длина стороны треугольника не может равняться нулю или быть отрицательным");
+                }
+                double trSquare = (aSide * bSide) / 2;
+                Console.WriteLine("Таким образом площадь треугольника равно {0:0.0} см в квадрате", trSquare);
+                Console.ReadKey();
+                return trSquare;
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+                return 0;
+            }
+        }
+    }
+}
---
+++ ExerciseTriangleTests/GetTriangleSquareTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExerciseTriangle;
+
+namespace ExerciseTriangleTests
+{
+    [TestClass]
+    public class GetTriangleSquareTests
+    {
+        [TestMethod]
+        public void TestGetTriangleSquare()
+        {
+            double a = 6;
+            double b = 7;
+            double expected = 21;
+            double actual;
+
+            actual = GetTriangleSquare.TriangleSquare(a, b);
+            Assert.AreEqual(expected, actual, "Тест не проqден");
+        }
+    }
+}
